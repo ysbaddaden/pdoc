6db31d52b07a17b8c278429b215c477d4e976034
ysbaddaden <ysbaddaden@gmail.com>
2009-06-23 11:59:27 +0200

    Added --help parameter.

--- lib/pdoc.php
+++ lib/pdoc.php
@@ -21,12 +21,22 @@ for ($n = 1; $n < $_SERVER['argc']; $n++)
     if (strpos($arg, '=')) {
       list($arg, $value) = explode('=', $arg);
     }
+    elseif (isset($_SERVER['argv'][++$n])) {
+      $value = $_SERVER['argv'][$n];
+    }
     else {
-      $value = $_SERVER['argv'][++$n];
+      $value = null;
     }
     
     switch($arg)
     {
+      case 'help': 
+        echo "Usage: pdoc [options] [input_directory] [output_directory]\n";
+        echo "Options:\n";
+        echo "  --help           Displays this help message.\n";
+        echo "  --project name   Sets the project's name.\n";
+        echo "  --exclude path/  Excludes a file or directory from documentation.\n";
+      break;
       case 'exclude': $excludes[] = $value; break;
       case 'project': $project = $value; break;
       default: trigger_error("Unknown parameter: --{$arg}\n");
