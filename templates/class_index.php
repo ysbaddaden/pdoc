62ca55bcd886e10cbeaed9f4cbe03c9d2b502106
Julien <ysbaddaden@gmail.com>
2009-10-08 12:12:16 +0200

    Missed a file in previous commit.

---
+++ templates/class_index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta name="language" content="en"/>
+  <title>Documentation for <?= $this->project_name ?></title>
+	<link rel="stylesheet" type="text/css" charset="utf-8" href="style.css"/>
+</head>
+<body>
+<div id="main">
+
+  <header>
+    <a href="index.html">Documentation for <strong><?= $this->project_name ?></strong></a>
+  </header>
+  
+  <section>
+    <h1>List of classes</h1>
+    
+    <ul class="class-index">
+      <? foreach($this->parser->classes as $klass): ?>
+        <li><a href="<?= klass_url($klass) ?>"><?= $klass['name'] ?></a></li>
+      <? endforeach; ?>
+    </ul>
+  </section>
+  
+</div>
+</body>
+</html>
